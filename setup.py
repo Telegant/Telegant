2c317e81c6035f144f09af8180c23459dea3039c
Kotov584 <85634618+Kotov584@users.noreply.github.com>
2023-03-19 23:47:39 -0600

    Bug fixes, code improvements, snake_case and PascalCase for methods 0.1.9

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md'),
 
 setup(
     name='Telegant',
-    version='0.1.8',
+    version='0.1.9',
     description='An Elegant Modern Bot Framework for Python',
     long_description=long_description,
     long_description_content_type='text/markdown',
